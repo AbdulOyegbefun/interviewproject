aaa353a6194c1cc627ab2b7fb5dea78efae40c6b
Abdulateef Oyegbefun <abdulateef.oyegbefun@utexas.edu>
2023-05-11 00:34:46 -0500

    procfile

--- interview_app.py
+++ interview_app.py
@@ -68,3 +68,4 @@ def tdidt_classifier(tree, header, instance):
 if __name__ == "__main__":
     port = os.environ.get("PORT", 5000)
     app.run(debug=False, host="0.0.0.0",port=port)
+    #app.run(debug=True)
